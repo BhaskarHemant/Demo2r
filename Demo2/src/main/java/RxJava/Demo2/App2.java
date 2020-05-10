c5948650dce1200882c28145b97c821e4d07dd61
HDB <abc@gmail.com>
2020-05-09 20:12:53 -0700

    c2

--- Demo2/src/main/java/RxJava/Demo2/App2.java
+++ Demo2/src/main/java/RxJava/Demo2/App2.java
@@ -26,19 +26,10 @@ public class App2 {
 		System.out.println("local final");
 	}
 	
-<<<<<<< HEAD
-=======
-
->>>>>>> branch 'master' of https://github.com/BhaskarHemant/Demo2r.git
 	public void localMaster() {
 		System.out.println("local master");
 	}
-	
-<<<<<<< HEAD
-=======
 
-
->>>>>>> branch 'master' of https://github.com/BhaskarHemant/Demo2r.git
 	public void remoteMaster2() {
 		System.out.println("remote master2");
 	}
