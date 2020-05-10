002183591f27cd8383e3772ef69285d9f2a415b6
HDB <abc@gmail.com>
2020-05-09 19:19:09 -0700

    first commit

---
+++ Demo2/src/main/java/RxJava/Demo2/App.java
@@ -0,0 +1,13 @@
+package RxJava.Demo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
